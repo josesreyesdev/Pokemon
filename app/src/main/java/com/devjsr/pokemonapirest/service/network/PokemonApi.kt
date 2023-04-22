ae9c2b47f4925aff2b9664643385f5c57752e0f4
Jose SR <josesanchez09190@gmail.com>
2023-04-22 13:21:08 -0600

    Upgrade gradles

---
+++ app/src/main/java/com/devjsr/pokemonapirest/service/network/PokemonApi.kt
@@ -0,0 +1,23 @@
+package com.devjsr.pokemonapirest.service.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+private const val BASE_URL_POKEMON = "https://pokeapi.co/api/v2/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL_POKEMON)
+    .build()
+
+object PokemonApi {
+    val retrofitService : PokemonApiService by lazy {
+        retrofit.create(PokemonApiService::class.java)
+    }
+}
--- app/src/main/java/com/devjsr/pokemonapirest/service/network/PokemonApiService.kt
+++ app/src/main/java/com/devjsr/pokemonapirest/service/network/PokemonApiService.kt
@@ -2,25 +2,10 @@ package com.devjsr.pokemonapirest.service.network
 
 import com.devjsr.pokemonapirest.modelApi.PokemonListResponse
 import com.devjsr.pokemonapirest.modelApi.Pokemon
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
-
-private const val BASE_URL_POKEMON = "https://pokeapi.co/api/v2/"
-
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
-
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
-    .baseUrl(BASE_URL_POKEMON)
-    .build()
 
 interface PokemonApiService {
     @GET("pokemon")
@@ -33,10 +18,4 @@ interface PokemonApiService {
     suspend fun getPokemonByName(
         @Path("name") name: String
     ) : Response<Pokemon>
-}
-
-object PokemonApi {
-    val retrofitService : PokemonApiService by lazy {
-        retrofit.create(PokemonApiService::class.java)
-    }
 }
